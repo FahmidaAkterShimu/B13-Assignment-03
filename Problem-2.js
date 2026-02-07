a9f9f80afc303956a98516d3e3dc388fe14d415f
Fahmida Akter Shimu <fahmidaaktershimu@gmail.com>
2026-02-07 21:15:51 +0600

    Problem-2.js

---
+++ Problem-2.js
@@ -0,0 +1,13 @@
+function validOtp(otp) {
+    if (typeof otp !== "string") {
+        return "Invalid";
+    }
+    else if (otp.length === 8 && otp.startsWith("ph-")) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+console.log(validOtp("ph-10985"));
